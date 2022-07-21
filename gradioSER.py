4f6c1323865902f223820e80da21562cf00ef457
okayshahi <shahidahmohd@gmail.com>
2022-07-21 09:20:11 +0800

    removed record

--- gradioSER.py
+++ gradioSER.py
@@ -48,7 +48,7 @@ def SER(audio, accuracy, numberEmotions):
         print("Features extraction successful with", feature.shape[1], "features") # backend shows extraction numbers
         predict = True
     except:
-        emotion = "Error...Please Upload or Record Audio!" # no audio provided
+        emotion = "Error...Please Upload Audio!" # no audio provided
 
     if numberEmotions == "6 emotions": # if number of emotions selected was 6 emotions
         model = getmodel("AUGEMO6") # get the 6 emotions model
@@ -72,20 +72,16 @@ def SER(audio, accuracy, numberEmotions):
     return emotion, img # return emotion and accuracy score chart
 
 # inputs
-heading = "Speech Emotion Recognition" # heading in the webpage
+heading = "Speech Emotion Recognition System" # heading in the webpage
 userAudio = gr.inputs.Audio(source="upload", type="file", label="Upload to recognize the emotion!") # upload an audio
-userAudioMIC = gr.inputs.Audio(source="microphone", type="file", label="Start Recording to recognize emotion!") # record  an audio
 imageAccuracy = gr.inputs.Radio(["Accuracy Testing Score", "Accuracy Training Score"], label="Select the chart to see CREMA-D accuracy score of diffierent algorithms:") # choose accuracy chart
 numberEmotions = gr.inputs.Dropdown(["6 emotions", "5 emotions (excluding disgust)", "4 emotions (excluding disgust, neutral)", "3 emotions (excluding disgust, neutral, angry)"], label="Select the number of emotions:") # choose number of emotions
 
 # outputs
 prediction = gr.outputs.Label(label="Emotion Recognized is") # output of uploaded audio
-predictionMIC = gr.outputs.Label(label="Emotion Recognized is") # output of recorded audio
 predictionACC = gr.outputs.Image(label="Chart produced") # output of chart selected
 
 # interfaces
 description = "A Speech Emotion Recognition for Culturally Diverse English Speakers to recognize Happy, Sad, Fear, Angry, Neutral, and Disgust emotions."
 uploadAudio = gr.Interface(fn=SER, inputs=[userAudio, imageAccuracy, numberEmotions], outputs=[prediction, predictionACC], title=heading, description=description, allow_flagging="never") # upload interface
-recordAudio = gr.Interface(fn=SER, inputs=[userAudioMIC, imageAccuracy, numberEmotions], outputs=[predictionMIC, predictionACC], title=heading, description=description, allow_flagging="never")  # record interface
-demo = gr.TabbedInterface([uploadAudio, recordAudio], ["Upload an audio", "Record an audio"]) # tabbed both interfaces
-demo.launch() # launches interface on web-browser
+uploadAudio.launch() # launches interface on web-browser
